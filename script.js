d28f4cb91fa39b521a2c2c1b5506dbd5bfd3a57d
vcleonc <88452663+vcleonc@users.noreply.github.com>
2021-08-29 22:25:10 -0500

    Peer-graded Assignment

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount  = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if(principal<=0){
+        alert("Enter a positive number")
+        document.getElementById("principal").focus()
+        return false
+    }
+
+    //document.getElementById("result").innerHTML="If you deposit "+ principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
     
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of 
+    <mark>${rate}%</mark>.<br>You will receive an amount of <mark>${amount.toFixed(2)}</mark>,<br>in the year <mark>${year}</mark><br>`
 }
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+'%';
+}
+
         
